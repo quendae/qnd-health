808e997f6a83f15941183f446dfc7fac739a15bd
quendae <qnd.000@gmail.com>
2026-09-21 12:42:06 +0200

    style: load insight view styles

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './features.css';
+import './insights.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
